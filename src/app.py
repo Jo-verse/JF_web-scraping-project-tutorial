75beb9f37b32c69f2b5ff0e01a2bdd370b51e589
Jo-verse <fernandesjoana8@gmail.com>
2025-02-26 17:13:19 +0000

    web_scrapping

--- src/app.py
+++ src/app.py
@@ -6,3 +6,12 @@ import sqlite3
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+url = "https://companies-market-cap-copy.vercel.app/index.html"
+
+# Download HTML
+response = requests.get(url)
+if response.status_code != 200:
+    raise Exception(f"Error al acceder a la página: {response.status_code}")
+html_content = response.text
+
+html_content
